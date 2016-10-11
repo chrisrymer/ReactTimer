283ce0ad96cd1cd11167295605a82ed477356d2e
chrisrymer <rymercho@gmail.com>
2016-10-11 21:21:56 +0100

    Add countdown functionality

---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// TODO: Add React.createClass() style
+var Timer = (props) => {
+    return (
+        <div>
+            Timer.jsx Rendered
+        </div>
+    )
+};
+
+module.exports = Timer;
