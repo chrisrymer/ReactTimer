c31b8ada99d86986f59a291b5f2de6dd6e107f39
chrisrymer <rymercho@gmail.com>
2017-04-05 09:13:10 +0100

    Updates test scripts todo

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -3,5 +3,6 @@ var expect = require('expect');
 describe('App', () => {
     it('should properly run tests', () => {
         expect(1).toBe(1);
+        //TODO: Tests to complete
     });
-});
+});git
